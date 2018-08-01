4c267560d89dfd024f90937cc100b172fbf25f68
Aritra Nag <aritranag89@gmail.com>
2018-08-01 11:04:37 +0530

    Code Commit

--- src/main/java/org/davromalc/tutorial/repository/CustomerRepository.java
+++ src/main/java/org/davromalc/tutorial/repository/CustomerRepository.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.davromalc.tutorial.model.Customer;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface CustomerRepository extends MongoRepository<Customer, String> {
 
     public Customer findByFirstName(String firstName);
     public List<Customer> findByLastName(String lastName);
+    @Query("{ 'country' : ?0 }")
     public List<Customer> findByCountry(String country);
 
 }
